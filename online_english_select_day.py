d5a80baab37e55ddb6032b3ac4bf420ccc8e71fd
Akimasa Liu <liumingzheng8@outlook.com>
2021-03-06 14:08:08 +0900

    Updated path to chromedriver

--- online_english_select_day.py
+++ online_english_select_day.py
@@ -8,7 +8,7 @@ end=False
 days=["Mon","Wed","Fri"]
 
 
-driver=webdriver.Chrome(executable_path="./../chromedriver.exe")
+driver=webdriver.Chrome(executable_path="./../chromedriver_win32/chromedriver.exe")
 loginurl="https://ost.benesse.ne.jp/ol/member/login/login_ost"
 driver.get(loginurl)
 customerId=driver.find_element_by_id("customerId")
